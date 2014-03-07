9215ebd333343437a936897a108ef9cda4a44efd
gnawhleinad <danielleehwang@gmail.com>
2014-03-06 19:28:30 -0800

    add base phabricator set-up

---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "precise-current-amd64"
+  config.vm.box_url = "http://cloud-images.ubuntu.com/precise/current/precise-server-cloudimg-vagrant-amd64-disk1.box"
+
+  config.vm.hostname = "phabricator-p4"
+
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+
+  config.vm.provision :shell, :path => "install.sh"
+end
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# modified script at http://www.phabricator.com/rsrc/install/install_ubuntu.sh
+
+mkdir /opt/phabricator-p4
+cd /opt/phabricator-p4
+
+sudo apt-get -qq update
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password bigfix'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password bigfix'
+sudo apt-get -y install mysql-server
+
+sudo apt-get -y install \
+  git apache2 dpkg-dev \
+  php5 php5-mysql php5-gd php5-dev php5-curl php-apc php5-cli php5-json
+
+sudo apt-get -y install emacs
+
+sudo a2enmod rewrite
+
+apt-get source php5
+PHP5=`ls -1F | grep '^php5-.*/$'`
+(cd $PHP5/ext/pcntl && phpize && ./configure && make && sudo make install)
+
+git clone git://github.com/facebook/libphutil.git
+git clone git://github.com/facebook/arcanist.git
+git clone git://github.com/facebook/phabricator.git
+
+cd -
+
+sudo cp -f /vagrant/httpd.conf /etc/apache2/httpd.conf
+sudo chown root:root /etc/apache2/httpd.conf
+sudo chmod 644 /etc/apache2/httpd.conf
+
+sudo sed -i 's/^;date.timezone =/date.timezone = America\/Los_Angeles/' /etc/php5/apache2/php.ini
+
+sudo service apache2 restart
+
+sudo service ufw stop
+
+sudo /opt/phabricator-p4/phabricator/bin/config set mysql.user root
+sudo /opt/phabricator-p4/phabricator/bin/config set mysql.pass bigfix
+sudo /opt/phabricator-p4/phabricator/bin/storage upgrade --force
+
+sudo /opt/phabricator-p4/phabricator/bin/config set phabricator.base-uri 'http://phabricator-p4.ohana:8080/'
+
+sudo /opt/phabricator-p4/phabricator/bin/phd start
+
+sudo chown -R vagrant:vagrant /opt/phabricator-p4
+
+sudo sed -i '/.*skip-external.*/asql-mode=STRICT_ALL_TABLES' /etc/mysql/my.cnf
+sudo service mysql restart
