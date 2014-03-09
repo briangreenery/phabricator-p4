fc4a363bd806e849b8701505573df8354ccadcb9
Brian Green <briangreenery@gmail.com>
2014-03-08 18:40:22 -0800

    Create a p4 client in the provisioner

--- install-p4.sh
+++ install-p4.sh
@@ -29,6 +29,24 @@ wget http://filehost.perforce.com/perforce/r13.3/bin.linux26x86_64/p4
 chmod +x p4d p4
 mv p4 /usr/local/bin
 
-# Set environment
+# Create vagrant p4 user
 
-echo 'export P4PORT=phabricator-p4.ohana:1666' >> /home/vagrant/.profile
+export P4PORT=phabricator-p4.ohana:1666
+p4 client -i << OHANA_MEANS_FAMILY
+
+Client: phabricator-p4
+Update: 2014/03/08 22:09:40
+Access: 2014/03/08 22:09:40
+Owner: vagrant
+Host: phabricator-p4
+Root: /home/vagrant/depot
+Options: noallwrite noclobber nocompress unlocked nomodtime normdir
+SubmitOptions: submitunchanged
+LineEnd: local
+View: //depot/... //phabricator-p4/...
+OHANA_MEANS_FAMILY
+
+# Prepare vagrant user's environment
+
+echo "export P4PORT=$P4PORT" >> /home/vagrant/.profile
+mkdir /home/vagrant/depot
